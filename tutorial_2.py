6d4c17183dbec88ebe306b60fc391f9a615a3e78
= <=>
2023-05-10 09:11:27 +0800

    Final commit

--- tutorial_2.py
+++ tutorial_2.py
@@ -4,10 +4,10 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 # render templated pages on website
-@app.route('/')
-def home():
+@app.route('/<name>/')
+def home(name):
     
-    return render_template('index.html')
+    return render_template('index.html', content=name, r=2, list=['bill','john','tim'])
 
 if __name__ == "__main__":
     app.run()
