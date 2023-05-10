b1406683b6a4473d745e81a9d98374944c312986
= <=>
2023-05-10 08:40:18 +0800

    tutorial 2 commit

--- tutorial_2.py
+++ tutorial_2.py
@@ -3,9 +3,10 @@ from flask import Flask, redirect, url_for, render_template
 # create instance of Flask
 app = Flask(__name__)
 
-# define pages on website
+# render templated pages on website
 @app.route('/')
 def home():
+    
     return render_template('index.html')
 
 if __name__ == "__main__":
