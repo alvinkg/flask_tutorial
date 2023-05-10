0cdb86e07aa8cbc4abfec05be31b1c586d03202b
= <=>
2023-05-10 11:06:41 +0800

    1. cleaned up route and mtd home 2. add config 'debug=True'

--- tutorial_3.py
+++ tutorial_3.py
@@ -4,10 +4,10 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 # render templated pages on website
-@app.route('/<name>/')
-def home(name):
+@app.route('/')
+def home():
     
-    return render_template('index.html', content=name, r=2, list=['bill','john','tim'])
+    return render_template('index.html' )
 
 if __name__ == "__main__":
     # Add debug=True to auto restart upon changes
