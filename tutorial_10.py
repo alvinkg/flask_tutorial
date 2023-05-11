c4318cc04a20bfe6293b6a454e1f43208be9440f
= <=>
2023-05-11 13:50:52 +0800

    add divisional / app blueprint

--- tutorial_10.py
+++ tutorial_10.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 from tenth import tenth
+from admin.admin import admin
 
 # instantiate Flask app
 app = Flask(__name__)
 
-# register blueprint 'second'
+# register blueprint 'tenth'
 app.register_blueprint(tenth, url_prefix="/tenth/")
+app.register_blueprint(admin, url_prefix="/admin/")
 
 # define route
 @app.route("/")
