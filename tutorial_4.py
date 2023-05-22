864efc9eafa43804118e051905704c9e1187b607
= <=>
2023-05-22 14:33:52 +0800

    tweaked for index4.html

--- tutorial_4.py
+++ tutorial_4.py
@@ -4,24 +4,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index4.html')
+    return "Welcome"
 
 # login page exposing a list of two mtd 'get, post'
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
-    if request.method == "POST":
+    
+    if request.method == 'POST':
         user = request.form['name']
+        # if there is any entry
         if user:
-            return redirect(url_for('user', usr=user))
-        else:
-            return render_template('login.html')
+            return redirect(url_for('user', user=user))
     else:
-        return render_template('login.html')
+        return render_template('index4.html')
 
 # user page
-@app.route("/<usr>/")
-def user(usr):
-    return f"<h1> {usr} </h1>"
+@app.route("/<user>/")
+def user(user):
+    return f"<h1> {user} </h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
