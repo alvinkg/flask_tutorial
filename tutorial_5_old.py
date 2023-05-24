1729254ef0d9c57255aeba2985e73e13a81797a7
= <=>
2023-05-24 11:53:30 +0800

    old tutorial 5

---
+++ tutorial_5_old.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, redirect, url_for, request, session
+from datetime import timedelta
+ 
+# instantiate a new Flask app
+app = Flask(__name__)
+
+# Session data is encrypted on the server and needs a secret key
+app.secret_key = 'hello'
+app.permanent_session_lifetime = timedelta(minutes=5)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login/', methods = ['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form['name']
+        session["user"] = user    
+        if user:
+            return redirect(url_for('user'))
+        else:
+            return render_template('login.html')
+    else:
+        if 'user' in session:
+            return redirect(url_for('user'))
+        return render_template('login.html')
+    
+@app.route('/user/')
+def user():
+    if "user" in session:
+        user = session['user']
+        return f'<h1>{user}</h1>'
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/logout/')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login')) 
+
+if __name__ == '__main__':
+    app.run(debug=True)
